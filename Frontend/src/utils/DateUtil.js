cc155e9f637569d88faf0c4489e424974409e5a9
Pranay <praveenpranay07@gmail.com>
2026-01-05 12:37:05 +0530

    Removed frontend folder to separate frontend deployment

--- Frontend/src/utils/DateUtil.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateStr) => {
-  return new Date(dateStr).toLocaleDateString("en-IN", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  });
-};
--- Frontend/src/utils/ModelUtil.js
+++
@@ -1,7 +0,0 @@
-export const todoItemToClientModel = (serverItem) => {
-  return {
-    id: serverItem.id,
-    todoText: serverItem.task,
-    todoDate: serverItem.date
-  }
-}
